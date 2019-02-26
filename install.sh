2e6da861ec26a7683bf8c486c4332ccea1636cc6
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2019-02-26 15:47:49 +0100

    Updated with instructions to get automatic updates from github

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Cloning software from github..."
+git clone https://github.com/Dans-labs/ontology-mapping-chatbot
+echo "Downloading data..."
 curl https://eu.ftp.opendatasoft.com/bmoreau/data_dumps.zip -L -o data_dumps.zip
 unzip -o data_dumps.zip
 rm data_dumps.zip
@@ -7,7 +10,6 @@ rm -r __MACOSX/
 
 if [ ! -f ontology-mapping-chatbot/chatbot_app/local_settings.py ]
 then
-    #touch ontology-mapping-chatbot/chatbot_app/local_settings.py
     cp ontology-mapping-chatbot/chatbot_app/settings.py ontology-mapping-chatbot/chatbot_app/local_settings.py
     echo "SECRET_KEY = '<SECRET_KEY>'" >> ontology-mapping-chatbot/chatbot_app/local_settings.py
     echo "ALLOWED_HOSTS = ['localhost', '127.0.0.1']" >> ontology-mapping-chatbot/chatbot_app/local_settings.py
